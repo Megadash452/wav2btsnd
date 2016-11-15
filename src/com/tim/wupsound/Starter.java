2bfc5c668dab212652a7cce92409ace28398a0f9
Zarklord1 <Bestgoaltenderever@gmail.com>
2016-11-15 14:00:27 -0500

    discovered that the 0x0002 at the begining of the btsnd is not the channel count

--- src/com/tim/wupsound/Starter.java
+++ src/com/tim/wupsound/Starter.java
@@ -14,10 +14,9 @@ public class Starter {
     	//btsnd is BIG_ENDIAN wav is LITTLE_ENDIAN     
         System.out.println("WAV to btsnd 0.1 alpha");
         final byte[] wav_header1 = new byte[]{0x52,0x49,0x46,0x46};
-        final byte[] wav_header2 = new byte[]{0x57,0x41,0x56,0x45,0x66,0x6D,0x74,0x20,0x10,0x00,0x00,0x00,0x01,0x00};
-        final byte[] wav_header3 = new byte[]{(byte) 0x80,(byte) 0xBB,0x00,0x00};
-        final byte[] wav_header4 = new byte[]{0x10,0x00,0x64,0x61,0x74,0x61};
-        final int avgBytesPerSec = 96000;
+        final byte[] wav_header2 = new byte[]{0x57,0x41,0x56,0x45,0x66,0x6D,0x74,0x20
+        ,0x10,0x00,0x00,0x00,0x01,0x00,0x02,0x00,(byte) 0x80,(byte) 0xBB,0x00,0x00,0x00
+        ,(byte) 0xEE,0x02,0x00,0x04,0x00,0x10,0x00,0x64,0x61,0x74,0x61};
         
         if(args.length == 0){
         	System.out.println("Usage:");
@@ -79,37 +78,16 @@ public class Starter {
 	            byte[] compare_buffer2 = new byte[wav_header2.length];            
 	            buffer.position(0x08);
 	            buffer.get(compare_buffer2);
-	               
-	            buffer.position(0x16);
-	            int channels = buffer.order(ByteOrder.LITTLE_ENDIAN).getShort();
-	            
-	            byte[] compare_buffer3 = new byte[wav_header3.length];            
-	            buffer.position(0x18);
-	            buffer.get(compare_buffer3);
-	               
-	            buffer.position(0x1C);
-	            int bytesPerSec = buffer.order(ByteOrder.LITTLE_ENDIAN).getInt();
-	               
-	            buffer.position(0x20);
-	            int blockSize = buffer.order(ByteOrder.LITTLE_ENDIAN).getShort();
-	            
-	            byte[] compare_buffer4 = new byte[wav_header4.length];            
-	            buffer.position(0x22);
-	            buffer.get(compare_buffer4);
 	            
 	            if(!( Arrays.equals(compare_buffer1,wav_header1) &&
-	                  Arrays.equals(compare_buffer2,wav_header2) &&
-	                  Arrays.equals(compare_buffer3,wav_header3) &&
-	                  Arrays.equals(compare_buffer4,wav_header4) &&
-	                  ((avgBytesPerSec*channels) == bytesPerSec) &&
-		              (channels*2) == blockSize) ){
+	                  Arrays.equals(compare_buffer2,wav_header2) )){
 	                exitWithError("badinfile");
 	            }
 	            
 	            ByteBuffer output = ByteBuffer.allocate(data.length - 0x2C + 8);
-	            output.put(ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(channels).array());
+	            output.put(ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(2).array());
 	            if(silentLoop) {
-	            	loopPoint = (((data.length - 0x2C) / (channels * 2)) + 1);
+	            	loopPoint = (((data.length - 0x2C) / 4) + 1);
 	            }
 	            output.put(ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(loopPoint).array());
 	            for(int i = 0x2C;i<data.length;i+=2){
@@ -138,23 +116,11 @@ public class Starter {
 	            byte[] data = Files.readAllBytes(path);
 	            ByteBuffer buffer = ByteBuffer.allocate(data.length);
 	            buffer.put(data);
-
-	            buffer.position(0);
-	            int channels = buffer.order(ByteOrder.BIG_ENDIAN).getInt();
-	            
-	            buffer.position(4);
-	            @SuppressWarnings("unused")
-				int loopPointStart = buffer.order(ByteOrder.BIG_ENDIAN).getInt();
 	            
 	            ByteBuffer output = ByteBuffer.allocate(data.length - 8 + 0x2C);
 	            output.put(wav_header1);
 	            output.put(ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(data.length - 8 + 0x2C).array());
 	            output.put(wav_header2);
-	            output.put(ByteBuffer.allocate(2).order(ByteOrder.LITTLE_ENDIAN).putShort((short)channels).array());
-	            output.put(wav_header3);
-	            output.put(ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(channels*avgBytesPerSec).array());
-	            output.put(ByteBuffer.allocate(2).order(ByteOrder.LITTLE_ENDIAN).putShort((short)(channels*2)).array());
-	            output.put(wav_header4);
 	            output.put(ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(data.length - 8).array());
 	            for(int i = 8;i<data.length;i+=2){
 	                short cur = buffer.order(ByteOrder.BIG_ENDIAN).getShort(i);
@@ -176,7 +142,7 @@ public class Starter {
      		System.out.println("you must use -in and provide a file to convert!");
      		System.exit(0);
      	} else if(reason.equals("badinfile")) {
-    		System.out.println("You need to provide a 48000khz 16bit .wav as input.");
+    		System.out.println("You need to provide a 48000khz 16bit stereo .wav as input.");
     		System.exit(0);
     	} else if(reason.equals("multipleloopargs")) {
     		System.out.println("You cant provide 2 loop arguments!");
