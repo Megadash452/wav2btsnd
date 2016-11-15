02f72fd0ed7ab4a6a48a6c3252eebdc3f5d0576d
Zarklord1 <Bestgoaltenderever@gmail.com>
2016-11-14 22:47:00 -0500

    added a -noloop option to make btsnd's that dont loop

--- src/com/tim/wupsound/Starter.java
+++ src/com/tim/wupsound/Starter.java
@@ -49,6 +49,16 @@ public class Starter {
             
             FileOutputStream fos = new FileOutputStream("bootSound.btsnd");
             fos.write(output.array());
+            if(args.length == 2){
+            	System.out.println(args[1]);
+                if(args[1].equals("-noloop")) {
+                	byte[] silenceloop = new byte[]{0x00,0x00,0x00,0x00};
+                	int loopStart = (((int)fos.getChannel().position() -8) / 4);
+                	fos.write(silenceloop);
+                	fos.getChannel().position(4);
+                	fos.write(ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(loopStart).array());
+                }
+            }
             fos.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -58,7 +68,7 @@ public class Starter {
     }
     
     public static void exitWithError(){
-        System.out.println("You need to provide a 48000khz 16bit steroe .wav as input");
+        System.out.println("You need to provide a 48000khz 16bit stereo .wav as input");
         System.exit(0);
     }
     
